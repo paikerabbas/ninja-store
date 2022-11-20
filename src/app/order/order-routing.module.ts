24c9294e9f66e0d1634ba40d82a64dd54d345dd3
Ratnakar Paswan <ratnakar15799@gmail.com>
2022-11-20 20:24:42 +0530

    created neew order module

--- src/app/order/order-routing.module.ts
+++ src/app/order/order-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderDashboardComponent } from './order-dashboard/order-dashboard.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: OrderDetailComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrderRoutingModule } from './order-routing.module';
+import { OrderDashboardComponent } from './order-dashboard/order-dashboard.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OrderDashboardComponent,
+    OrderDetailComponent
+  ],
   imports: [
     CommonModule,
     OrderRoutingModule
