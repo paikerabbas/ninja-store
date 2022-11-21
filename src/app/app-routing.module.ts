36be2f0e5c842b8ea68d6e2b9ba4d147e0ca5533
Amar Husain <amar.husain156@gmail.com>
2022-11-21 19:33:22 +0530

    created header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './core/login-page/login-page.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent},
+  { path: 'login', component: LoginPageComponent},
+  { path: 'about', loadChildren: () => 
+      import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'contact', loadChildren: () => 
+      import('./contact/contact.module').then(m => m.ContactModule) },
+  { path: '', redirectTo:'/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { LoginPageComponent } from './core/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
-  ],
+    FooterComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
+    LoginPageComponent, 
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
