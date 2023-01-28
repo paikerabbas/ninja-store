909ffe389a188152c739025c63c7e8b4641bbde5
Amar Husain <amar.husain156@gmail.com>
2023-01-28 14:43:42 +0530

    created components

--- src/app/men-topwear/men-blazer/men-blazer.module.ts
+++ src/app/men-topwear/men-blazer/men-blazer.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenblazerDashboardComponent } from './menblazer-dashboard/menblazer-dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MenblazerDashboardComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/men-watch/menwatch-dashboard/menwatch-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menwatch-dashboard',
+  templateUrl: './menwatch-dashboard.component.html',
+  styleUrls: ['./menwatch-dashboard.component.scss']
+})
+export class MenwatchDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
